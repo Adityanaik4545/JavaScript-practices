0e7be591b27d182991ab2cf22a346df470cba54b
Adityanaik4545 <adityank23@outlook.com>
2025-04-29 00:03:25 +0530

    created an prank hacking website(which shows the visitors that their data is being stolen)

---
+++ hacking page/script.js
@@ -0,0 +1,37 @@
+async function addItems(item) {
+    await delay();
+    let div = document.createElement("div");
+    div.innerHTML = item;
+    document.body.append(div)
+}
+
+async function startHacking() {
+    setInterval(()=>{
+        let last=document.body.lastElementChild;
+        if (last.innerHTML.endsWith("...")) {
+            last.innerHTML=last.innerHTML.slice(0,last.innerHTML.length-3)
+        }
+        else{
+
+            last.innerHTML=last.innerHTML+"."
+        }
+
+    },100)
+    let text = ["initializing hacking",
+         "initialized hacking now reading your data",
+          "reading your files",
+           "Password files detected",
+            "Sending all personal files and Passwords to server", 
+            "Cleaning up"];
+    for(let item of text){
+        await addItems(item);
+    }
+    
+}
+startHacking()
+function delay() {
+    return new Promise(resolve => {
+        let timeOut = Math.random() * 5 + 1;
+        setTimeout(resolve, timeOut * 1000);
+    })
+}
